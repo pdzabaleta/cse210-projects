f0ea8de18ae06a528ed066a36b85747af69e4959
pdzabaleta <pdzabaleta@gmail.com>
2024-04-25 21:09:59 -0400

    Ask the user program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Thisis a C# program, Enjoy it!");
+        
+        // Ask the user for their name
+        string firstname, lastname;
+        Console.Write("What is your first name?");
+        firstname = Console.ReadLine();
+        Console.Write("What is your last name?");
+        lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is, {lastname}, {firstname} {lastname}");
     }
 }
---
+++ practice.cs
@@ -0,0 +1,4 @@
+
+Console.Write("What is your favorite color? ");
+string color = Console.ReadLine();
+Console.Write($"Your color is {color}" );
