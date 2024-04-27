dc02a7dc144977a457a63f339fc9104fdbcfb744
pdzabaleta <pdzabaleta@gmail.com>
2024-04-26 22:33:41 -0400

    Guessing a random number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        
+        Console.WriteLine("This is a C# document. ENJOY IT! :) ");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.WriteLine("Guess the magic number!");
+        
+
+        
+
+        int numberGuessed = -1;
+        
+       do{
+        Console.Write("What is your guess?: ");
+        string gessNumber = Console.ReadLine();
+        numberGuessed = int.Parse(gessNumber);
+       
+        if (number > numberGuessed){
+            Console.WriteLine("Higher");
+        }
+
+        else if (number == numberGuessed) {
+            Console.WriteLine("You guessed it!");
+        }
+
+        else{
+            Console.WriteLine("Lower");
+        }
+       }while (number != numberGuessed);
     }
 }
