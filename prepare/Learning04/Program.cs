edcebfd747ff51ef0a09b9a081351a627aef3e50
pdzabaleta <pdzabaleta@gmail.com>
2024-05-17 21:42:01 -0400

    w04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment name1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(name1.GetSumary());
+
+        MathAssignment name2 = new MathAssignment("Sam Pockett", "Addition", "7.2", "3-9" );
+        Console.WriteLine(name2.GetSumary());
+        Console.WriteLine(name2.GetHomeworkList());
+
+        WritingAssignment name3 = new WritingAssignment("Cat Valentine", "Psychology",  "Stand Up");
+        Console.WriteLine(name3.GetSumary());
+        Console.WriteLine(name3.GetWritingInformation());
     }
 }
