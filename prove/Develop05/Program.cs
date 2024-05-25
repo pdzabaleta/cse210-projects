3d870c5bbc048eea5e7ecb4c9d84649afc6bf4f1
pdzabaleta <pdzabaleta@gmail.com>
2024-05-24 22:41:21 -0400

    w05

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,5 +1,6 @@
 using System;
-// To my program, I gave the ability to provide the total score with option 1, load goals with option 2, and load detailed goals with option 3, as a creativity criterion.
+// To my program, I gave the ability to provide the total score with option 1, load goals with option 2, and 
+// load detailed goals with option 3, as a creativity criterion.
 class Program
 {
     static void Main(string[] args)
